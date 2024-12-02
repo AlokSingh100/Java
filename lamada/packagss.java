b61a29fa92cc5d6768a6216103ac8e54e1083580
Alok Singh <159884599+AlokSingh100@users.noreply.github.com>
2024-12-02 17:31:58 +0530

    Add files via upload

--- lamada/packagss.java
+++ lamada/packagss.java
@@ -4,19 +4,46 @@ import java.util.Scanner;
 
 public class packagss {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What do you want to calculate");
+        System.out.println("1. addiction");
+        System.out.println("2. subcration");
+        System.out.println("3. division");
+        System.out.println("4. multiply");
+        int chose = scanner.nextInt();
+        calcil ba = null;
+        switch (chose) {
+            case 1:
+                ba = (double a, double b) -> {
+                    System.out.println("Addicional " + (a + b));
 
-            calcil ba=(double a,double b)->{
-                System.out.println("Addicional "+a+b);
-                System.out.println("Sibcrition "+(a-b));
-                System.out.println("Multiply "+a*b);
-                System.out.println("Devide "+a/b);
+                };
+                break;
+            case 2:
+                ba = (double a, double b) -> {
+                    System.out.println("Sibcrition " + (a - b));
 
-            };
-        Scanner scanner=new Scanner(System.in);
+                };
+                break;
+            case 3:
+                ba = (double a, double b) -> {
+
+                    System.out.println("Multiply " + a * b);
+
+                };
+                break;
+            case 4:
+                ba = (double a, double b) -> {
+
+                    System.out.println("Devide " + a / b);
+                };
+                break;
+        }
         System.out.println("Enter the First Number");
-        double a=scanner.nextDouble();
+        double a = scanner.nextDouble();
         System.out.println("Enter the Second Number");
-        double b=scanner.nextDouble();
-    ba.calculate( a, b);
+        double b = scanner.nextDouble();
+        ba.calculate(a, b);
+
     }
 }
